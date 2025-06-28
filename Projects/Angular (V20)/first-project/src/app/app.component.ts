16f62dd67aaf99e2ecd6c928fd9e07fc9756eb5f
pushkarpc <pushkarpchavan@gmail.com>
2025-06-28 22:59:38 +0530

    my first professional web-app for my portfolio

---
+++ Projects/Angular (V20)/first-project/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+// import { SidebarComponent } from '../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,
+            HeaderComponent,
+            FooterComponent,
+            // SidebarComponent
+          ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'first-project';
+}
+
